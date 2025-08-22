6d6c9749ed9072f066b124c6c80b396cf9b6d1d1
IRAWAN <codeirawan@gmail.com>
2025-08-22 23:03:02 +0700

    Create shuffle.ts

---
+++ src/utils/shuffle.ts
@@ -0,0 +1,9 @@
+// utils/shuffle.ts
+export function shuffleArray<T>(array: T[]): T[] {
+  const newArray = [...array]; // copy to avoid mutating original
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+  return newArray;
+}
